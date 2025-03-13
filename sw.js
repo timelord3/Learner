f45514556b7949066076330ea23951faedd743f6
timelord3 <106898257+timelord3@users.noreply.github.com>
2025-03-14 09:32:20 +1100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,70 @@
+const VERSION = "v1";
+const CACHE_NAME = `learner-hours-${VERSION}`;
+
+const APP_STATIC_RESOURCES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/LearnerHours.json",
+  "/icons/wheel.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      // Open the specified cache.
+      const cache = await caches.open(CACHE_NAME);  
+      // Add all static files to the cache.
+      cache.addAll(APP_STATIC_RESOURCES);   
+    })(), // Immediately invoke the async function.
+  );
+});
+
+// delete old caches on activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      // Get a list of all the caches
+      const names = await caches.keys();
+      // Go through the list
+      await Promise.all(
+        names.map((name) => {
+          // Check if it is not the current cache
+          if (name !== CACHE_NAME) {
+	    // delete it
+            return caches.delete(name);
+          }
+        }),
+      );
+      // Set the current service worker as the controller
+      await clients.claim();
+    })(),
+  );
+});
+
+// On fetch, intercept server requests
+// and respond with cached responses instead of going to network
+self.addEventListener("fetch", (event) => {
+  // As a single page app, direct app to always go to cached home page.
+  if (event.request.mode === "navigate") {  // Looking for a web page
+    event.respondWith(caches.match("/"));
+    return;
+  }
+
+  // For all other requests, go to the cache first, and then the network.
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request.url);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
+      }
+      // If resource isn't in the cache, return a 404.
+      return new Response(null, { status: 404 });
+    })(),
+  );
+});
+
+
